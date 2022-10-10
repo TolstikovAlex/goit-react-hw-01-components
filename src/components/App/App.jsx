a27c7a365961fefd8478baf3c13d6ab6fc632baa
Alex Tolstikov <tolstikovalexander2@gmail.com>
2022-10-10 22:03:13 +0300

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from 'components/profile/Profile';
 import user from 'components/profile/user.json';
 import { Statistics } from 'components/statistics/Statistics';
 import data from 'components/statistics/data.json';
-import { FriendList } from 'components/FriendList/FriendList';
-import friends from 'components/FriendList/friends.json';
-import { TransactionHistory } from 'components/Transactions/TransactionHistory';
-import transactions from 'components/Transactions/transactions.json';
+import { FriendList } from 'components/friendList/FriendList';
+import friends from 'components/friendList/friends.json';
+import { TransactionHistory } from 'components/transactions/TransactionHistory';
+import transactions from 'components/transactions/transactions.json';
 import { Container } from './App.styled';
 
 export const App = () => {
